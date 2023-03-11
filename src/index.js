30f666ff017750c06c6442086b4153b6a2f7cdc9
stevehu <stevehuinnz@gmail.com>
2023-03-11 23:50:46 +1300

    l

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './index.css'
 
-//list 
-const songs = [
-    { id: 1, name: 'monkey dance' },
-    { id: 2, name: 'what makes you beautiful' },
-    { id: 3, name: 'fade to black' },
-]
+const list = ( < h1 className = "title" >
+        william and kaitlyn < /h1>)
 
-const list = ( <
-        ul > {
-            songs.map(item => < li > { item.name } < /li>)} <
-                /ul>
-            )
-            //rendering elements 
-            ReactDOM.render(list, document.getElementById('root'))
+
+        //rendering elements 
+        ReactDOM.render(list, document.getElementById('root'))
