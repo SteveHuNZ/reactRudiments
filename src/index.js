62cefcad56a687466b2aef307d19f2ded7bae866
stevehu <stevehuinnz@gmail.com>
2023-03-15 21:31:30 +1300

    s

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import './index.css'
 
 const list = ( < h1 className = "title" >
-        william and kaitlyn < /h1>)
+        william and kaitlyn 同的的 的 < /h1>)
 
 
         //rendering elements 
